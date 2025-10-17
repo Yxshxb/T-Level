773d5ad15c395daf54ce126707f923dc98d019c4
Dayton Faulkner <24000979@student.stamford.ac.uk>
2025-10-17 09:38:53 +0100

    update

---
+++ pokemon_api 17.10.25/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$url = "https://pokeapi.co/api/v2/pokemon/eevee";
+
+$response = file_get_contents($url);
+
+$data = json_decode($response, true);
+
+
+$name = $data['name'];
+
+
+$sprite = $data['sprites']['front_default'];
+
+
+echo "Name: " . $name . "<br>";
+echo "Sprite: <img src='" . $sprite . "' alt='" . $name . "'>";
+?>
